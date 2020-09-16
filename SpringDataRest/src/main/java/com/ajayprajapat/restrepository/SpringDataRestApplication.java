a9cc402a0833191bee6e1ce07dd5419d41dcdc64
Ajay Prajapat <ajstylox@gmail.com>
2020-09-16 08:42:28 +0530

    rename package

--- SpringDataRest/src/main/java/com/ajayprajapat/restrepository/SpringDataRestApplication.java
+++ SpringDataRest/src/main/java/com/ajayprajapat/restrepository/SpringDataRestApplication.java
@@ -1,4 +1,4 @@
-package com.ajayprajapt.restrepository;
+package com.ajayprajapat.restrepository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
