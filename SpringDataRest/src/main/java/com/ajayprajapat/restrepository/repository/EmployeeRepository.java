1e4ddda5f3e35a9f68b8f7c3526b91e553d77643
Ajay Prajapat <ajstylox@gmail.com>
2020-09-16 08:58:24 +0530

    customize rest url

--- SpringDataRest/src/main/java/com/ajayprajapat/restrepository/repository/EmployeeRepository.java
+++ SpringDataRest/src/main/java/com/ajayprajapat/restrepository/repository/EmployeeRepository.java
@@ -1,11 +1,11 @@
 package com.ajayprajapat.restrepository.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ajayprajapat.restrepository.entity.EmployeeEntity;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "employees", path = "employee-list")
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
 }
